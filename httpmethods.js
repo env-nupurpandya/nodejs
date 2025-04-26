4537b8b1a72e42b8edfbfdf0a470527a9da7181c
env-nupurpandya <env.nupurpandya@outlook.com>
2025-04-26 15:54:37 +0530

    pagination implemented in get endpoint

--- httpmethods.js
+++ httpmethods.js
@@ -48,6 +48,37 @@ app.delete('/users/:id', (req, res) => {
     res.json(deletedUser[0]);
 });
 
+
+// Pagination
+
+// GET /users endpoint with pagination
+// http://localhost:3000/users-pagination?page=7&limit=3
+app.get('/users-pagination', (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+
+    if (page <= 0 || limit <= 0) {
+        return res.status(400).json({ message: 'Invalid page or limit value' });
+    }
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = startIndex + limit;
+
+    const paginatedUsers = mockData.slice(startIndex, endIndex);
+
+    if (paginatedUsers.length === 0) {
+        return res.status(404).json({ message: 'No users found for this page' });
+    }
+
+    res.json({
+        page,
+        limit,
+        totalUsers: mockData.length,
+        totalPages: Math.ceil(mockData.length / limit),
+        users: paginatedUsers
+    });
+});
+
 // Start server
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
